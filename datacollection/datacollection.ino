a2509e9c5ccd526abbc968cb10ca09d6f18018b8
ShadowPhoenix007 <96228746+ShadowPhoenix007@users.noreply.github.com>
2024-12-14 09:13:51 +0530

    Changed MPU code + added PID to smooth it
    adafruit library wasnt working properly so decided to go with the MPU one by electronic cats
    Added PID to smooth the values, makes it easier to differentiate between tremors and non-tremors

--- datacollection/datacollection.ino
+++ datacollection/datacollection.ino
@@ -2,48 +2,52 @@
 #define flex2 A1
 #define flex3 A2
 #define flex4 A3
-#define piezo A6
-#define flex5 A7
+#define piezo A7
+#define flex5 A6
 
 
-#include <Adafruit_MPU6050.h>
-#include <Adafruit_Sensor.h>
-#include <Wire.h>
+#include "Wire.h"
+#include "I2Cdev.h"
+#include "MPU6050.h"
 
-Adafruit_MPU6050 mpu;
+MPU6050 mpu;
 
 float acceleration[3] = {};
 float gyro[3] = {};
 float temperature = 0;
 
 
-  int flex1i = 0;
-  int flex2i = 0;
-   int flex3i = 0;
-   int flex4i = 0;
-   int flex5i = 0;
    int piezoi;
    int flexA[5] = {0,0,0,0,0};
    int flexMax[5] = {0,0,0,0,0};
    int flexMin[5] = {0,0,0,0,0};
 
+   float accelError[3] = {0, 0, 0};
+float gyroError[3] = {0, 0, 0};
+float accelLastError[3] = {0, 0, 0};
+float gyroLastError[3] = {0, 0, 0};
+float accelIntegral[3] = {0, 0, 0};
+float gyroIntegral[3] = {0, 0, 0};
+
+//pid
+float accelPID[3] = {0, 0, 0};
+float gyroPID[3] = {0, 0, 0};
+
 // the setup routine runs once when you press reset:
 void setup() {
-  Serial.begin(9600);
-
-  
   Serial.begin(115200);
-  if (!mpu.begin()) {
-    Serial.println("Failed to find MPU6050 chip");
-    while (1) {
-      delay(10);
-    }
-  }
-  Serial.println("MPU6050 Found!");
 
-  mpu.setAccelerometerRange(MPU6050_RANGE_8_G); //could be 2,4,8, or 16
-  mpu.setGyroRange(MPU6050_RANGE_500_DEG); //could be 250, 500, 1000, or 2000
-  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ); //could be 260, 184, 94, 44, 21, 10, or 5
+    Wire.begin();
+  Serial.begin(115200);
+  Serial.println("Initializing MPU6050...");
+  mpu.initialize();
+  if (mpu.testConnection()) {
+    Serial.println("MPU6050 connection successful!");
+  } else {
+    Serial.println("MPU6050 connection failed!");
+  }
+
+  Serial.println("MPU6050 Found!");
 
   Serial.println("Flex in max positions in ");
   delay(1000);
@@ -118,16 +122,29 @@ void loop() {
 
   Read();
   // // print out the value you read:
-  printiArray(flexA, 5, false);
-  printfArray(acceleration, 3, false);
-  printfArray(gyro, 3, false);
-  Serial.print(temperature);
+  //printiArray(flexA, 5, false);
+  printfArray(accelPID, 3, false);
+  printfArray(gyroPID, 3, true);
   Serial.println();
   delay(100);
 }
 
+float kp = 25;
+float ki = 0.1;
+float kd = 15;
+float PID(float setpoint, float measurement, float& integral, float& lastError)
+{
+  float error = setpoint - measurement;
+  float proportional = kp * error;
+  float derivative = kd * (error - lastError);
+  lastError = error;
+  integral = integral + error;
+  float finIntegral = integral * ki;
+  return (proportional + finIntegral + derivative);
+}
+
 void Read()
-{ 
+{
   flexA[0] = analogRead(flex1);
   flexA[1] = analogRead(flex2);
   flexA[2] = analogRead(flex3);
@@ -136,19 +153,24 @@ void Read()
   
   piezoi = analogRead(piezo);
 
-  sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp);
+  int16_t ax, ay, az, gx, gy, gz;
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
 
+  acceleration[0] = ax; //m/s^2
+  acceleration[1] = ay;
+  acceleration[2] = az;
 
-  acceleration[0] = a.acceleration.x; //m/s^2
-  acceleration[1] = a.acceleration.y;
-  acceleration[2] = a.acceleration.z;
+  gyro[0] = gx; //rad/s
+  gyro[1] = gy;
+  gyro[2] = gz;
 
-  gyro[0] = g.gyro.x; //rad/s
-  gyro[1] = g.gyro.y;
-  gyro[2] = g.gyro.z;
+  for (int i = 0; i < 3; i++) {
+    accelPID[i] = PID(0, acceleration[i], accelLastError[i], accelIntegral[i]);
+  }
 
-  temperature = temp.temperature;
-
+  // Apply PID to gyroscope data
+  for (int i = 0; i < 3; i++) {
+    gyroPID[i] = PID(0, gyro[i], gyroLastError[i], gyroIntegral[i]);
+  }
 }
 
